e82a2674cea265558ea040761b420ca480fc5c55
Paul <64870518+woodcock3@users.noreply.github.com>
2020-09-15 05:16:20 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -10,52 +10,49 @@ $.extend($.easing,
     }
 });
 
-(function myScroller( $ ) {
+(function( $ ) {
 
     var settings;
     var disableScrollFn = false;
     var navItems;
     var navs = {}, sections = {};
-	
-    if (navItems.id === "movenav") {
 
-      $.fn.navScroller = function(options) {
-          settings = $.extend({
-              scrollToOffset: 170,
-              scrollSpeed: 800,
-              activateParentNode: true,
-          }, options );
-          navItems = this;
+    $.fn.navScroller = function(options) {
+        settings = $.extend({
+            scrollToOffset: 170,
+            scrollSpeed: 800,
+            activateParentNode: true,
+        }, options );
+        navItems = this;
 
-          //attatch click listeners
-    	  navItems.on('click', function(event){
-      		  event.preventDefault();
-              var navID = $(this).attr("href").substring(1);
-              disableScrollFn = true;
-              activateNav(navID);
-              populateDestinations(); //recalculate these!
-          	  $('html,body').animate({scrollTop: sections[navID] - settings.scrollToOffset},
-                  settings.scrollSpeed, "easeInOutExpo", function(){
-                      disableScrollFn = false;
-                  }
-              );
-    	  });
+        //attatch click listeners
+    	navItems.on('click', function(event){
+    		event.preventDefault();
+            var navID = $(this).attr("href").substring(1);
+            disableScrollFn = true;
+            activateNav(navID);
+            populateDestinations(); //recalculate these!
+        	$('html,body').animate({scrollTop: sections[navID] - settings.scrollToOffset},
+                settings.scrollSpeed, "easeInOutExpo", function(){
+                    disableScrollFn = false;
+                }
+            );
+    	});
 
-          //populate lookup of clicable elements and destination sections
-          populateDestinations(); //should also be run on browser resize, btw
+        //populate lookup of clicable elements and destination sections
+        populateDestinations(); //should also be run on browser resize, btw
 
-          // setup scroll listener
-          $(document).scroll(function(){
-              if (disableScrollFn) { return; }
-              var page_height = $(window).height();
-              var pos = $(this).scrollTop();
-              for (i in sections) {
-                  if ((pos + settings.scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
-                      activateNav(i);
-                  }
-              }
+        // setup scroll listener
+        $(document).scroll(function(){
+            if (disableScrollFn) { return; }
+            var page_height = $(window).height();
+            var pos = $(this).scrollTop();
+            for (i in sections) {
+                if ((pos + settings.scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
+                    activateNav(i);
+                }
+            }
         });
-      }
     };
 
     function populateDestinations() {
@@ -154,4 +151,3 @@ $(document).ready(function (){
     });
   });
 });
-
